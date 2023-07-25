4bd46054196adae28dab1b24f751574b3b13562a
Rjk-Jami <rjkhan.1221@gmail.com>
2023-07-26 00:36:46 +0600

    reverse Function

---
+++ reverseFucntion.js
@@ -0,0 +1,12 @@
+const reverseString=(input)=> {
+  let reversedString = '';
+  for (let i = input.length - 1; i >= 0; i--) {
+    reversedString += input[i];
+  }
+  return reversedString;
+}
+
+
+const input = "hello world";
+const output = reverseString(input);
+console.log(output); // Output: "dlrow olleh"
