27acc93f0253369b30962d2e404f06a3a69b72d5
Rjk-Jami <rjkhan.1221@gmail.com>
2023-07-26 13:07:05 +0600

    task7RomanToInt

---
+++ task7RomanToInt.js
@@ -0,0 +1,36 @@
+function romanToInt(roman) {
+    const romanNumerals = {
+      I: 1,
+      V: 5,
+      X: 10,
+      L: 50,
+      C: 100,
+      D: 500,
+      M: 1000,
+    };
+  
+    let total = 0;
+    let prevValue = 0;
+  
+    for (let i = roman.length - 1; i >= 0; i--) {
+      const currentValue = romanNumerals[roman[i]];
+        // console.log(currentValue)
+      if (currentValue < prevValue) {
+        total -= currentValue;
+      } else {
+        total += currentValue;
+      }
+  
+      prevValue = currentValue;
+    }
+  
+    return total;
+  }
+  
+  // Test examples:
+  console.log(romanToInt("III")); 
+  console.log(romanToInt("IV")); 
+  console.log(romanToInt("IX")); 
+  console.log(romanToInt("XXI")); 
+  console.log(romanToInt("MDCLCMXCIV")); 
+  
